9cd07041230c480229b87279d19aeb897ab169e0
James Royalty <jroyalty@gmail.com>
2013-02-13 16:02:30 -0500

    Stub utility class for matrices.

---
+++ src/main/java/com/hackoeur/jglm/Matrices.java
@@ -0,0 +1,41 @@
+/* Copyright (C) 2013 James L. Royalty
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hackoeur.jglm;
+
+/**
+ * @author James Royalty
+ */
+public final class Matrices
+{
+	public static Mat4 perspective(final float fovy, final float aspect, final float zNear, final float zFar)
+	{
+		return null;
+	}
+	
+	public static Mat4 frustum(final float left, final float right, final float bottom, final float top, final float nearVal, final float farVal)
+	{
+		return null;
+	}
+	
+	public static Mat4 ortho(final float left, final float right, final float bottom, final float top, final float zNear, final float zFar)
+	{
+		return null;
+	}
+	
+	public static Mat4 ortho2d(final float left, final float right, final float bottom, final float top)
+	{
+		return null;
+	}
+}
