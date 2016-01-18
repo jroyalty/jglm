6159b128884cc92cdbb41d765d3b5071be1c0483
Giuseppe Barbieri <elect86@gmail.com>
2016-01-18 18:37:32 +0100

    Update Vec3.java

--- src/main/java/com/hackoeur/jglm/Vec3.java
+++ src/main/java/com/hackoeur/jglm/Vec3.java
@@ -1,15 +1,1 @@
-	public static final Vec3 VEC3_ZERO = new Vec3();
-	
-	final float x, y, z;
-	
-	public Vec3() {
-		this.x = 0f;
-		this.y = 0f;
-		this.z = 0f;
-	}
-	
-	public Vec3(final float x, final float y, final float z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
+try
